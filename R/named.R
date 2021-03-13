e4231748cf24faebfa1979083829da2e1f9ec71b
David Hugh-Jones <davidhughjones@gmail.com>
2021-03-13 10:22:54 +0000

    Add named_not_in() and named_not_like()

--- R/named.R
+++ R/named.R
@@ -22,39 +22,64 @@ NULL
 #' * unmatched elements of `y` do not return an `NA` element;
 #' * elements are returned in their original order within `x`.
 #'
+#' `named_except(x, y)` returns elements of `x` whose name is not
+#' an element of `y`.
+#'
 #' @return
-#' ```
-#' x[names(x) %in% y]
-#' ```
+#'
+#' For `named_in`: `x[names(x) %in% y]`.
+#'
+#' For `named_not_in`: `x[! names(x) %in% y]`.
+#'
 #' @export
 #'
 #' @examples
 #'
 #' vec <- c(one = 1, two = 2, three = 3, four = 4)
 #' named_in(vec, c("two", "one", "three", "five"))
+#' named_not_in(vec, c("two", "three"))
 named_in <- function (x, y) {
   x[names(x) %in% y]
+}
+
+
+#' @export
+#' @rdname named_in
+named_not_in <- function (x, y) {
+  x[! names(x) %in% y]
 }
 
 
 #' Subset objects by name using a regular expression
 #'
 #' @inherit doc-common
+#' @param ... Passed in to [grepl()].
 #'
 #' @return
-#' ```
-#' x[grepl(pattern, names(x))]
-#' ```
+#'
+#' For `named_like`: `x[grepl(pattern, names(x), ...)]`.
+#'
+#' For `named_not_like`: `x[! grepl(pattern, names(x), ...)]`.
+#'
 #' @export
 #'
 #' @examples
 #'
 #' vec <- c(one = 1, two = 2, three = 3, four = 4)
 #' named_like(vec, "^t")
+#' named_not_like(vec, "e$")
 #'
-named_like <- function (x, pattern) {
+named_like <- function (x, pattern, ...) {
   stopifnot(length(pattern) == 1)
-  x[grepl(pattern, names(x))]
+  x[grepl(pattern, names(x), ...)]
+}
+
+
+#' @rdname named_like
+#' @export
+named_not_like <- function (x, pattern, ...) {
+  stopifnot(length(pattern) == 1)
+  x[! grepl(pattern, names(x), ...)]
 }
 
 
